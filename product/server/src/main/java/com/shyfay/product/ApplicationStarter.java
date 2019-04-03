7c256ced3ca14f99cd96628d17318ab5b590c4e0
git-mx <464293746@qq.com>
2019-04-03 19:36:21 +0800

    瞎J8搞

--- product/server/src/main/java/com/shyfay/product/ApplicationStarter.java
+++ product/server/src/main/java/com/shyfay/product/ApplicationStarter.java
@@ -2,14 +2,20 @@ package com.shyfay.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ApplicationStarter {
+public class ApplicationStarter extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStarter.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApplicationStarter.class);
+    }
 }
